152b1b2fc912d95ef893c56939b0d4f8c081187a
quintenbogaert <quint.bogaert@gmail.com>
2025-10-29 15:16:00 +0100

    .

--- __init__.py
+++ __init__.py
@@ -1,16 +1,24 @@
 from flask import Flask
-from .models import db
 from .config import Config
+from .models import db
+from flask_migrate import Migrate
+
+migrate = Migrate()    
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # DB & Migrate initialiseren
     db.init_app(app)
+    migrate.init_app(app, db)  
 
+    # Zorg dat modellen geladen zijn vóór migraties draaien
     with app.app_context():
-        db.create_all()  # Create sql tables for our data models
+        from . import models   # belangrijk voor auto-generatie van migraties
+        # db.create_all()  VERWIJDERd — migraties nemen dit over
 
+    # Blueprints registreren
     from .routes import main
     app.register_blueprint(main)
 
